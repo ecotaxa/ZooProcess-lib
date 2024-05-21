3dd77cdea41a9f23eaec84da2a76686d0ddb8509
Galvagno Sébastien <dev@galvagno.info>
2024-05-21 15:05:42 +0200

    added substract

--- test_background.py
+++ test_background.py
@@ -1,7 +1,9 @@
 import unittest
 import pytest
+import numpy as np
 
 from pathlib import Path
+import cv2
 
 from Background import Background
 from ProjectClass import ProjectClass
@@ -26,7 +28,7 @@ from img_tools import (
     )
 
 
-class test_backgroung(unittest.TestCase):
+class test_background(unittest.TestCase):
 
     project_folder = "Zooscan_apero_tha_bioness_sup2000_sn033"
     TP = ProjectClass(project_folder)
@@ -85,22 +87,28 @@ class test_backgroung(unittest.TestCase):
 
         background.mean()
 
-    # @pytest.mark.skip(reason="Skipping this test for now because of XYZ reason.")  
+
+
+
+
+    @pytest.mark.skip(reason="Skipping this test for now because of XYZ reason.")  
     def test_all(self):
 
         back_name = "20240112_1518_back_large_1.tif" 
         back_file = Path(self.TP.back, back_name)
         print(f"file: {back_file}")
-        back_image = loadimage( back_file.as_posix())
+        back_image = rotate90c(loadimage( back_file.as_posix()))
         output_path = self.TP.testfolder
         border = Border(back_image)
         border.output_path = output_path
         border.name = back_name
-        # border.draw_image = loadimage(back_file.as_posix())
+        border.draw_image = rotate90c(loadimage(back_file.as_posix()))
 
-        # limitetop, limitbas, limitegauche, limitedroite = border.detect()
-        # image_back_unbordered = crop(back_image, left=limitetop, top=limitegauche, right=limitbas, bottom=limitedroite)
-        # saveimage(image_back_unbordered, back_name, "unbordered", ext="tiff", path=output_path)
+        limitetop, limitbas, limitegauche, limitedroite = border.detect()
+        print(f"back limite t={limitetop}, b={limitbas}, l={limitegauche}, r={limitedroite}")
+
+        image_back_unbordered = crop(back_image, left=limitetop, top=limitegauche, right=limitbas, bottom=limitedroite)
+        saveimage(image_back_unbordered, back_name, "unbordered", ext="tiff", path=output_path)
 
 
         # a faire apres
@@ -109,23 +117,47 @@ class test_backgroung(unittest.TestCase):
 
 
 
-        # sample = "apero2023_tha_bioness_sup2000_013_st46_d_n4_d1_1_sur_1"
-        # rawscan_file = Path(self.TP.rawscan, sample + "_raw" + "_1" + ".tif")
-        # image = loadimage(rawscan_file.as_posix())
-        # scan_border = Border(image)
-        # scan_border.output_path = output_path
-        # scan_border.name = sample
-        # scan_border.draw_image = loadimage(rawscan_file.as_posix())
+        sample = "apero2023_tha_bioness_sup2000_013_st46_d_n4_d1_1_sur_1"
+        rawscan_file = Path(self.TP.rawscan, sample + "_raw" + "_1" + ".tif")
+        image = loadimage(rawscan_file.as_posix())
+        scan_border = Border(image)
+        scan_border.output_path = output_path
+        scan_border.name = sample
+        scan_border.draw_image = loadimage(rawscan_file.as_posix())
 
-        # scan_limit_top, scan_limit_bottom, scan_limit_left, scan_limit_right = scan_border.detect()
-        # # limitetop, limitbas, limitegauche, limitedroite = scan_border.detect()
-        # # print(f"scan limite {scan_limite}")
-        # print(f"scan limite top={scan_limit_top}, b={scan_limit_bottom}, l={scan_limit_left}, r={scan_limit_right}")
+        scan_limit_top, scan_limit_bottom, scan_limit_left, scan_limit_right = scan_border.detect()
+        # limitetop, limitbas, limitegauche, limitedroite = scan_border.detect()
+        # print(f"scan limite {scan_limite}")
+        print(f"scan limite t={scan_limit_top}, b={scan_limit_bottom}, l={scan_limit_left}, r={scan_limit_right}")
+        print(f"scan limite top={scan_limit_top}, b={scan_limit_bottom}, l={scan_limit_left}, r={scan_limit_right}")
         
-        # scan_unbordered = cropnp(image, left=scan_limit_left, top=scan_limit_top, right=scan_limit_right, bottom=scan_limit_bottom)
-        # saveimage(scan_unbordered, sample, "unbordered", ext="tiff", path=output_path)
+        scan_unbordered = cropnp(image, left=scan_limit_left, top=scan_limit_top, right=scan_limit_right, bottom=scan_limit_bottom)
+        saveimage(scan_unbordered, sample, "unbordered", ext="tiff", path=output_path)
 
 
+        image_back_blurred = cv2.medianBlur(image_back_unbordered, 3)
 
+        H = scan_unbordered.shape[0]
+        L = scan_unbordered.shape[1]
+        # scale factor
+        # fx = 
+        # fy = 
+        interpolation = cv2.INTER_LINEAR
+        image_back_resized = cv2.resize( image_back_blurred, dsize=(L, H), interpolation=interpolation)
+        saveimage(image_back_resized, back_name, "resized", ext="tiff", path=output_path)
+
+        print(f"scan_unbordered shape: {scan_unbordered.shape}")
+        print(f"image_back_resized shape: {image_back_resized.shape}")
+
+        # image_substracted = scan_unbordered - image_back_resized
+        # image_substracted = cv2.subtract(scan_unbordered, image_back_resized)
+
+        # image_substracted = cv2.subtract(scan_unbordered, image_back_resized, dest=None, mask=None, dtype=np.uint8)
+        image_substracted = np.subtract(scan_unbordered, image_back_resized)
+        saveimage(image_substracted, sample, "substracted", ext="tiff", path=output_path)
+
+        image_substracted2 = np.subtract(image_back_resized, scan_unbordered)
+        saveimage(image_substracted2, sample, "substracted2", ext="tiff", path=output_path)
 
         # background.mean()
+
