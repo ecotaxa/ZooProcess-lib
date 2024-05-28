f95c24b039de24589cf1db325069e06c1a2f0ad5
Galvagno Sébastien <dev@galvagno.info>
2024-05-28 15:48:07 +0200

    old file to memo

---
+++ readlog.old.py
@@ -0,0 +1,34 @@
+from configparser import ConfigParser 
+
+from ProjectClass import ProjectClass
+
+# Malheureusement ne fonctionne pas
+# car le fichier n'est pas un vrai config file :(   
+
+class ReadLog:
+
+    def __init__(self, TP: ProjectClass, sample:str ) -> None:
+        
+        self.TP = TP
+        # sample = sample
+        logfile = self.TP.getLogFile(sample)
+
+        self.configur = ConfigParser()
+        self.configur.read(logfile)
+
+    def getBackgroundPattern(self) -> str:
+        self.configur.read(self.logfile)
+        # background = self.configur['Image_Process']['Background_correct_using']
+        background = self.configur.get('Image_Process','Background_correct_using')
+        print ("Installation Background_correct_using : ", background) 
+
+        return background
+        
+
+
+    # file = "t_17_2_tot_1_log.txt"
+
+
+
+    # configur = ConfigParser()
+    # print (configur.read('config.ini'))
