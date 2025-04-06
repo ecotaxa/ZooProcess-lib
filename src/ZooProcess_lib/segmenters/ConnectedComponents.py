perf(segmenter): optimize holes computation for ROI contours, results: min: 1.24 max: 24.24 mean: 3.17 median: 1.92 stddev: 2.44. Fix test bug in time stats.

src/ZooProcess_lib/segmenters/ConnectedComponents.py
@@ -54,6 +54,7 @@ class ConnectedComponentsSegmenter:
                 cls.prevent_inclusion(labels, holes, x, y, w, h)
                 filtering_stats[5] += 1
                 continue
+            # Horizontal stripes from scanner bed movement
             ratiobxby = w / h
             if ratiobxby > max_w_to_h_ratio:
                 filtering_stats[6] += 1
@@ -95,7 +96,7 @@ class ConnectedComponentsSegmenter:
             cv2.CC_STAT_HEIGHT,
             cv2.CC_STAT_AREA,
         ) == (0, 1, 2, 3, 4)
-        # Add index after stats, starting at 1 as the first 'component' is the whole image
+        # Add index after stats, starting at 1, as the first 'component' is the whole image.
         offs = 1
         indices = np.reshape(
             np.arange(start=offs, stop=len(cc_stats), dtype=np.uint32),
@@ -221,7 +222,99 @@ class ConnectedComponentsSegmenter:
         # elapsed = int((time.time() - before) * 10000)
         # if elapsed > 0:
         #     print("get_regions:", elapsed, " ratio ", empty_ratio, w, h)
+            holes, sub_mask = ConnectedComponentsSegmenter.get_regions_using_contours(
+                labels, cc_id, x, y, w, h, height, width
+            )
+        else:
+            holes, sub_mask = ConnectedComponentsSegmenter.get_regions_using_floodfill(labels, cc_id, x, y, w, h, width,
+                                                                                       height)
         return holes, sub_mask
+
+    @staticmethod
+    def get_regions_using_floodfill(labels:ndarray, cc_id:int, x, y, w, h, width, height):
+        if x == 0 or y == 0 or x + w == width or y + h == height:
+            # The shape touches an image border
+            sub_labels = cropnp(
+                image=labels, top=y, left=x, bottom=y + h, right=x + w
+            )
+            # noinspection PyUnresolvedReferences
+            obj_mask = (sub_labels == cc_id).astype(
+                dtype=np.uint8
+            ) * 255  # 0=not in shape (either around shape or inside), 255=shape
+            # Draw lines around for the floodFill to spread all around
+            obj_mask = cv2.copyMakeBorder(
+                obj_mask, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=(0,)
+            )
+        else:
+            # Wa can save the copyMakeBorder above by picking around the existing lines
+            sub_labels = cropnp(
+                image=labels,
+                top=y - 1,
+                left=x - 1,
+                bottom=y + h + 2,
+                right=x + w + 2,
+            )
+            # noinspection PyUnresolvedReferences
+            obj_mask = (sub_labels == cc_id).astype(
+                dtype=np.uint8
+            ) * 255  # 0=not in shape (either around shape or inside), 255=shape
+        holes = 255 - obj_mask  # 255=around 0=shape 255=holes
+        cv2.floodFill(
+            holes, mask=None, seedPoint=(0, 0), newVal=(0,)
+        )  # 0=around 0=shape 255=holes
+        sub_mask = cropnp(image=obj_mask, top=1, left=1, bottom=h + 1, right=w + 1)
+        holes = cropnp(image=holes, top=1, left=1, bottom=h + 1, right=w + 1)
+        return holes, sub_mask
+
+    @staticmethod
+    def get_regions_using_contours(
+        labels: ndarray, cc_id: int, x, y, w, h, height, width
+    ):
+        sub_labels = cropnp(image=labels, top=y, left=x, bottom=y + h, right=x + w)
+        # noinspection PyUnresolvedReferences
+        obj_mask = (sub_labels == cc_id).astype(
+            dtype=np.uint8
+        ) * 255  # 0=not in shape (either around shape or inside), 255=shape
+        contours, _ = cv2.findContours(
+            obj_mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE
+        )
+        # Despite all the efforts made to avoid it (e.g. small holes in border lines so contour detection algo
+        # can sneak inside particles area), sometimes the first contour is an _outer_ one. It can be fitting perfectly
+        # around the border lines to image borders, or, when there are tiny holes in border lines, all holes could be covered
+        # in by some particle or noise.
+        # Historical algorithm is immune to this problem, because when taking 2 slices, one vertical side is completely opened
+        # and contour detection will find everything. But it raises other issues like lost big particles.
+        #
+        # OTOH, sometimes there could be only 3 full borders, resulting in same contour, but an inner
+        # one as usually.
+        #
+        # First case manifest itself as an inner contour filling nearly all the image. It's geometrically OK as a
+        # hole inside the shape, but we get rid of it.
+        #
+        # ConnectedComponentsSegmenter.debug_cc_comp_contour(obj_mask, contours)
+        if x == 0 and y == 0 and x + w == width and y + h == height:
+            big_area_threshold = height * width * 8 / 10
+            to_remove = ConnectedComponentsSegmenter.find_contours_above(
+                contours, big_area_threshold
+            )
+            if to_remove is not None:
+                # print("4 borders")
+                contours = list(contours)
+                del contours[to_remove]
+        # Holes are in second level of RETR_CCOMP method output
+        holes = np.zeros_like(obj_mask)  # 0:non-hole 255:hole
+        cv2.drawContours(
+            image=holes,
+            contours=contours[1:],
+            contourIdx=-1,
+            color=(255,),
+            thickness=cv2.FILLED,  # FILLED -> inside + contour
+        )  # 0=not in hole, 255=hole
+        # Above 'holes' is not pixel-exact as the edges b/w object and holes is drawn, eliminate them
+        holes ^= obj_mask
+        sub_mask = obj_mask
+        return holes, sub_mask
+
 
     @staticmethod
     def find_contours_above(contours, big_area_threshold):

tests/test_sample.py
@@ -370,6 +370,20 @@ missingd = [
 
 more_than25_is_black = [(APERO1, "apero2023_tha_bioness_018_st66_n_n8_d3")]
 
+stripes_in_thresholded = more_than25_is_black + [
+    (APERO1, "apero2023_tha_bioness_018_st66_n_n6_d1_1_sur_1"),
+    (APERO1, "apero2023_tha_bioness_017_st66_d_n7_d2_2_sur_2"),
+    (APERO1, "apero2023_tha_bioness_017_st66_d_n4_d2_2_sur_2"),
+    (APERO1, "apero2023_tha_bioness_013_st46_d_n1_d1_1_sur_2"),
+]
+
+all_borders = [
+    (APERO1, "apero2023_tha_bioness_005_st20_d_n2_d2_3_sur_4"),
+    (APERO1, "apero2023_tha_bioness_005_st20_d_n2_d2_4_sur_4"),
+]
+
+very_big = [(APERO1, "apero2023_tha_bioness_006_st20_n_n7_d1_2_sur_2")]
+
 wrong_mask_maybe_gives_no_roi_when_legacy_has = [
     (APERO1, "apero2023_tha_bioness_013_st46_d_n1_d2_1_sur_1"),
 ]
@@ -456,20 +470,20 @@ def test_linear_response_time(projects, tmp_path):
         )
         spent_times.append(spent)
     np_times = np.array(spent_times)
-    min, max, avg, mean, stddev = [
+    min, max, mean, median, stddev = [
         round(m, 2)
         for m in (
             np.min(spent_times),
             np.max(spent_times),
             np.average(np_times),
-            np.mean(np_times),
+            np.median(np_times),
             np.std(np_times),
         )
     ]
-    print("min:", min, "max:", max, "avg:", avg, "mean:", mean, "stddev:", stddev)
-    z_scores = (np_times - mean) / stddev
-    max_zscore = 2
-    for a_sample, its_time, its_score in zip(test_set, spent_times, z_scores ):
+    print("min:", min, "max:", max, "mean:", mean, "median:", median, "stddev:", stddev)
+    z_scores = (np_times - median) / stddev
+    max_zscore = 3
+    for a_sample, its_time, its_score in zip(test_set, spent_times, z_scores):
         if abs(its_score) > max_zscore:
             print("Fails for sample:", a_sample, "time:", its_time, "score:", its_score)
     assert np.argmax(np.abs(z_scores) > max_zscore) == 0