65b4547e22cf6fd161ac74d6e428271f8c75801f
grololo06 <laurent.salinas@laposte.net>
2025-04-27 09:16:12 +0200

    feat(features):UT-like bg test

--- tests/data_dir.py
+++ tests/data_dir.py
@@ -6,3 +6,7 @@ IMAGES_DIR = DATA_DIR / "images"
 SEGMENTER_DIR = IMAGES_DIR / "segmenter"
 MEASURES_DIR = IMAGES_DIR / "measures"
 FEATURES_DIR = DATA_DIR / "features"
+PROJECT_DIR = DATA_DIR / "project"
+BACKGROUND_DIR = PROJECT_DIR / "background"
+
+BACK_TIME = "20240529_0946"
--- tests/test_combine_backgrounds.py
+++ tests/test_combine_backgrounds.py
@@ -1,27 +1,24 @@
+from pathlib import Path
+
 import cv2
 import numpy as np
 
-from ZooProcess_lib.ZooscanProject import ZooscanProject
 from ZooProcess_lib.Zooscan_combine_backgrounds import Zooscan_combine_backgrounds
 from ZooProcess_lib.img_tools import loadimage
-from tests.env_fixture import projects
-from tests.projects_for_test import APERO
+from data_dir import BACK_TIME, BACKGROUND_DIR
 
 
-def test_combine_backgrounds(projects, tmp_path):
+def test_combine_backgrounds(tmp_path):
     """Ensure we combine like legacy the scanned background images"""
-    project_folder = APERO
-    TP = ZooscanProject(projects, project_folder)
-    scan_date = "20241216_0926"
+    scan_date = BACK_TIME
     source_files = [
-        TP.getProcessedBackgroundFile(TP.getRawBackgroundFile(scan_date, index))
-        for index in (1, 2)
+        Path(BACKGROUND_DIR, f"{scan_date}_back_large_{index}.tif") for index in (1, 2)
     ]
     assert [source_file.exists() for source_file in source_files]
-    reference_bg_file = TP.getCombinedBackgroundFile(scan_date, "manual")
+    reference_bg_file = Path(BACKGROUND_DIR, f"{scan_date}_background_large_manual.tif")
     assert reference_bg_file.exists()
     output_path = tmp_path / reference_bg_file.name
-    Zooscan_combine_backgrounds(source_files, output_path, TP.readLut())
+    Zooscan_combine_backgrounds(source_files, output_path)
     expected_image = loadimage(reference_bg_file, type=cv2.IMREAD_UNCHANGED)
     actual_image = loadimage(output_path, type=cv2.IMREAD_UNCHANGED)
     assert np.array_equal(expected_image, actual_image)
