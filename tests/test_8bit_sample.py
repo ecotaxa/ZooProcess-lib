9654bda58a29afc1b3014e085c800199319f42e1
grololo06 <laurent.salinas@laposte.net>
2025-04-28 07:34:03 +0200

    test(processing):UT and field test of raw sample processing

---
+++ tests/test_8bit_sample.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+from ZooProcess_lib.ImageJLike import convert_16bit_image_to_8bit_min_max
+from ZooProcess_lib.Lut import Lut
+from ZooProcess_lib.Zooscan_convert import Zooscan_convert
+from ZooProcess_lib.img_tools import loadimage
+from data_dir import RAW_DIR, CONFIG_DIR, SAMPLE_DIR
+
+
+def test_identical_converted_8bit_sample(tmp_path):
+    """Ensure we convert like legacy the scanned background images"""
+    raw_sample_file = RAW_DIR / "apero2023_tha_bioness_017_st66_d_n1_d3_raw_1.tif"
+
+    output_path = tmp_path / raw_sample_file.name
+    lut = Lut.read(CONFIG_DIR / "lut.txt")
+    Zooscan_convert(raw_sample_file, output_path, lut)
+    actual_image = loadimage(output_path, type=cv2.IMREAD_UNCHANGED)
+
+    ref_8bit_sample_file = SAMPLE_DIR / "apero2023_tha_bioness_017_st66_d_n1_d3_1.tif"
+    assert ref_8bit_sample_file.exists()
+    expected_image = loadimage(ref_8bit_sample_file, type=cv2.IMREAD_UNCHANGED)
+
+    assert expected_image.shape == actual_image.shape
+    assert np.array_equal(expected_image, actual_image)
+
+
+def test_convert_16bit_image_to_8bit_min_max():
+    # Below min should en up in 0s
+    img = np.array([766, 762, 745, 778, 798, 790], np.uint16)
+    out = convert_16bit_image_to_8bit_min_max(img, 805, 65536)
+    assert out.tolist() == [0, 0, 0, 0, 0, 0]
--- tests_field/test_8bit_sample.py
+++ tests_field/test_8bit_sample.py
@@ -1,20 +1,30 @@
+import random
+
 import cv2
 import numpy as np
+import pytest
 
-from ZooProcess_lib.ImageJLike import convert_16bit_image_to_8bit_min_max
 from ZooProcess_lib.ZooscanFolder import ZooscanFolder
 from ZooProcess_lib.ZooscanProject import ZooscanProject
 from ZooProcess_lib.Zooscan_convert import Zooscan_convert
 from ZooProcess_lib.img_tools import loadimage
-from tests.env_fixture import projects
-from tests.projects_for_test import APERO2000
+from .env_fixture import projects
+from .projects_repository import tested_samples
+
+# The test takes ages, by randomizing the order there are better chances to see problems early
+shuffled = tested_samples.copy()
+random.shuffle(shuffled)
 
 
-def test_identical_converted_8bit_sample(projects, tmp_path):
+@pytest.mark.parametrize(
+    "project, sample",
+    shuffled,
+    ids=[sample for (_prj, sample) in shuffled],
+)
+def test_identical_converted_8bit_sample(projects, project, sample, tmp_path):
     """Ensure we convert like legacy the scanned background images"""
-    folder = ZooscanFolder(projects, APERO2000)
-    TP = ZooscanProject(projects, APERO2000)
-    sample = "apero2023_tha_bioness_sup2000_017_st66_d_n1_d2_3_sur_4"
+    folder = ZooscanFolder(projects, project)
+    TP = ZooscanProject(projects, project)
     index = 1  # TODO: should come from get_names() below
 
     raw_sample_file = folder.zooscan_scan.raw.get_file(sample, index)
@@ -31,10 +41,3 @@ def test_identical_converted_8bit_sample(projects, tmp_path):
 
     assert expected_image.shape == actual_image.shape
     assert np.array_equal(expected_image, actual_image)
-
-
-def test_convert_16bit_image_to_8bit_min_max():
-    # Below min should en up in 0s
-    img = np.array([766, 762, 745, 778, 798, 790], np.uint16)
-    out = convert_16bit_image_to_8bit_min_max(img, 805, 65536)
-    assert out.tolist() == [0, 0, 0, 0, 0, 0]
